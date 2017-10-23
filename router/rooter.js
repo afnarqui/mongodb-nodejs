8e68c7d29de1b7f2ad34e34bfbbe61d16fcd8be5
Andrés felipe naranjo quintero <afnarqui@hotmail.com>
2017-10-23 03:34:08 -0500

    inicial2

--- router/rooter.js
+++ router/rooter.js
@@ -2,9 +2,9 @@ var express = require('express')
 var datos = require('../models/ajustesVentasDiarias')
 var login = require('..//models/login')
 
-var rooter = express.Router()
+var router = express.Router()
 
-rooter.post('/datos', function(req, res, next) {
+router.post('/datos', function(req, res, next) {
     
     datos.create(req.body, function (err, post) {
       if (err) return next(err);
@@ -12,28 +12,28 @@ rooter.post('/datos', function(req, res, next) {
     });
   });
 
-  rooter.post('/login', function(req, res, next) {
+  router.post('/login', function(req, res, next) {
     login.create(req.body, function (err, post) {
       if (err) return next(err);
       res.json(post);
     });
   });
 
-  rooter.get('/login', function(req, res, next) {
+  router.get('/login', function(req, res, next) {
     login.find(function (err, login) {
       if (err) return next(err);
       res.json(login);
     });
   });
 
-  rooter.post('/cargos', function(req, res, next) {
+  router.post('/cargos', function(req, res, next) {
     login.create(req.body, function (err, post) {
       if (err) return next(err);
       res.json(post);
     });
   });
 
-  rooter.get('/cargos', function(req, res, next) {
+  router.get('/cargos', function(req, res, next) {
     login.find(function (err, login) {
       if (err) return next(err);
       res.json(login);
